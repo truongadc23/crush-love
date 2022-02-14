6587b077585b49f2b5d9ef09ef0a71aa0f2e92e6
truongadc23 <99649178+truongadc23@users.noreply.github.com>
2022-02-14 17:13:58 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn cậu vì đồng ý hihi",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý đi chơi mí tớ mà ^^ </br> Đợi tớ khỏe ra hà nội rui đi nhé </br> Hẹn cậu ngày gần nhất nhé . </br> Baii Baii Quỳnh Chang ',
+    'Tớ biết là cậu sẽ đồng ý đi chơi mí tớ mà ^^ </br> Đợi tớ khỏe ra hà nội rui đi nhé </br> Hẹn cậu ngày gần nhất hihi. </br> Baii Baii Quỳnh Chang ',
 };
